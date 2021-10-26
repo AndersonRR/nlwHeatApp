313677856dd6c94e02a3e9b27571cebe584eb7ad
anderson.felipe <anderson.felipe@unijui.edu.br>
2021-10-26 11:39:52 -0300

    Finaliza componente de envio de mensagens

--- src/components/SignInBox/styles.ts
+++ src/components/SignInBox/styles.ts
@@ -1,7 +1,11 @@
 import { StyleSheet } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const styles = StyleSheet.create({
   container: {
-
+    height: 48,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    marginBottom: getBottomSpace() + 20,
   }
 });
